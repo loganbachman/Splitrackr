94d1017743cf585fedd50a1ac34fddf54e6644ad
loganbachman <loganbachman04@gmail.com>
2025-09-30 02:40:45 -0700

    push yml change

--- backend/src/main/java/com/splitrackr/backend/auth/WebConfig.java
+++ backend/src/main/java/com/splitrackr/backend/auth/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${security.app.cors.allowed-origins}")
+    @Value("${app.cors.allowed-origins}")
     private String allowedOrigins;
 
     @Override
